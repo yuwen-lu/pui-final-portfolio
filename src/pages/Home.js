b1051589e21fc214fce8ffba2aee591892e8021e
Yuwen <wayne.lu1998@gmail.com>
2020-12-12 23:29:03 -0500

    Add text cloud, change to tsparticles (it's finally working)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import Particles from 'react-particles-js';
+// import Particles from 'react-particles-js';
+import Particles from "react-tsparticles";
 import { Container, Row, Col } from 'react-bootstrap';
 import { useMediaQuery } from 'react-responsive';
 import Header from '../layout/Header';
@@ -50,50 +51,274 @@ export default function Home() {
                     data-aos-duration="1000"
                     data-aos-easing="ease-in-out">
                     <Col id="skill-cloud" style={iconCloudStyle} md={6} sm={10}>
-                        <Particles 
-                            params={{
-                                particles: {
-                                    "number": {
-                                        "value": 5
+                        <Particles
+                            options={{
+                                    "autoPlay": true,
+                                    "detectRetina": true,
+                                    "fpsLimit": 30,
+                                    "infection": {
+                                        "cure": false,
+                                        "delay": 0,
+                                        "enable": false,
+                                        "infections": 0,
+                                        "stages": []
                                     },
-                                    "line_linked": {
-                                        "enable": false
-                                    },
-                                    shape: {
-                                        type: 'image',
-                                        "image": [
-                                            {
-                                                "src": process.env.PUBLIC_URL + "/icons/text-svg/behavioral-research.svg",
-                                            },
-                                            {
-                                                "src": process.env.PUBLIC_URL + "/icons/text-svg/user-centered-design.svg",
-                                            },
-                                            {
-                                                "src": process.env.PUBLIC_URL + "/icons/text-svg/design.svg",
-                                            },
-                                            {
-                                                "src": process.env.PUBLIC_URL + "/icons/linkedin.svg",
-                                            },
-                                            {
-                                                "src": process.env.PUBLIC_URL + "/icons/youtube.svg",
-                                            },
-                                            {
-                                                "src": process.env.PUBLIC_URL + "/icons/weibo.svg",
-                                            },
-                                        ]
-                                    },
-                                    "size": {
-                                        "value": 40,
-                                        "random": false,
-                                        "anim": {
+                                    "interactivity": {
+                                        "detectsOn": "canvas",
+                                        "events": {
+                                            "onClick": {
                                             "enable": true,
-                                            "speed": 1,
-                                            "size_min": 20,
-                                            "sync": false
+                                            "mode": "push"
+                                            },
+                                            "onDiv": {
+                                                "selectors": [],
+                                                "enable": false,
+                                                "mode": [],
+                                                "type": "circle"
+                                            },
+                                            "onHover": {
+                                                "enable": true,
+                                                "mode": "repulse",
+                                                "parallax": {
+                                                    "enable": false,
+                                                    "force": 60,
+                                                    "smooth": 10
+                                            }
+                                            },
+                                            "resize": true
+                                        },
+                                        "modes": {
+                                            "attract": {
+                                                "distance": 200,
+                                                "duration": 0.4,
+                                                "speed": 1
+                                            },
+                                            "bounce": {
+                                                "distance": 200
+                                            },
+                                            "bubble": {
+                                                "distance": 400,
+                                                "duration": 2,
+                                                "opacity": 0.8,
+                                                "size": 40
+                                            },
+                                            "connect": {
+                                                "distance": 80,
+                                                "links": {
+                                                    "opacity": 0.5
+                                                },
+                                                "radius": 60
+                                            },
+                                            "grab": {
+                                                "distance": 400,
+                                                "links": {
+                                                    "blink": false,
+                                                    "consent": false,
+                                                    "opacity": 1
+                                                }
+                                            },
+                                            "light": {
+                                                "area": {
+                                                    "gradient": {
+                                                        "start": {
+                                                            "value": "#ffffff"
+                                                        },
+                                                        "stop": {
+                                                            "value": "#000000"
+                                                        }
+                                                    },
+                                                    "radius": 1000
+                                                },
+                                                "shadow": {
+                                                    "color": {
+                                                        "value": "#000000"
+                                                    },
+                                                    "length": 2000
+                                                }
+                                            },
+                                            "push": {
+                                                "quantity": 4
+                                            },
+                                            "remove": {
+                                                "quantity": 2
+                                            },
+                                            "repulse": {
+                                                "distance": 100,
+                                                "duration": 0.6,
+                                                "speed": 1
+                                            },
+                                            "slow": {
+                                                "factor": 3,
+                                                "radius": 200
+                                            },
+                                            "trail": {
+                                                "delay": 1,
+                                                "quantity": 1
+                                            }
                                         }
-                                    }
-                                }
-                            }}/>
+                                    },
+                                    "manualParticles": [],
+                                    "motion": {
+                                        "disable": false,
+                                        "reduce": {
+                                            "factor": 4,
+                                            "value": false
+                                        }
+                                    },
+                                    "particles": {
+                                        "color": {
+                                            "value": "#ffffff",
+                                            "animation": {
+                                                "enable": false,
+                                                "speed": 1,
+                                                "sync": true
+                                            }
+                                        },
+                                        "move": {
+                                            "angle": {
+                                                "offset": 45,
+                                                "value": 90
+                                            },
+                                            "attract": {
+                                                "enable": false,
+                                                "rotate": {
+                                                    "x": 600,
+                                                    "y": 1200
+                                                }
+                                            },
+                                            "direction": "none",
+                                            "distance": 0,
+                                            "enable": true,
+                                            "gravity": {
+                                                "acceleration": 9.81,
+                                                "enable": false,
+                                                "maxSpeed": 50
+                                            },
+                                            "noise": {
+                                                "delay": {
+                                                    "random": {
+                                                    "enable": false,
+                                                    "minimumValue": 0
+                                                    },
+                                                    "value": 0
+                                                },
+                                                "enable": false
+                                            },
+                                            "outModes": {
+                                                "default": "out",
+                                                "bottom": "out",
+                                                "left": "out",
+                                                "right": "out",
+                                                "top": "out"
+                                            },
+                                            "random": false,
+                                            "size": false,
+                                            "speed": 2,
+                                            "straight": false,
+                                            "trail": {
+                                                "enable": false,
+                                                "length": 10,
+                                                "fillColor": {
+                                                    "value": "#000000"
+                                                }
+                                            },
+                                            "vibrate": false,
+                                            "warp": false
+                                        },
+                                        "number": {
+                                            "density": {
+                                            "enable": true,
+                                            "area": 800,
+                                            "factor": 1000
+                                            },
+                                            "limit": 0,
+                                            "value": 80
+                                        },
+                                        "opacity": {
+                                            "random": {
+                                            "enable": false,
+                                            "minimumValue": 0.1
+                                            },
+                                            "value": 0.5,
+                                            "animation": {
+                                            "enable": true,
+                                            "minimumValue": 0.1,
+                                            "speed": 1,
+                                            "sync": false
+                                            }
+                                        },
+                                        "reduceDuplicates": false,
+                                        "rotate": {
+                                            "random": {
+                                            "enable": false,
+                                            "minimumValue": 0
+                                            },
+                                            "value": 0,
+                                            "animation": {
+                                            "enable": false,
+                                            "speed": 0,
+                                            "sync": false
+                                            },
+                                            "direction": "clockwise",
+                                            "path": false
+                                        },
+                                        "shadow": {
+                                            "blur": 0,
+                                            "color": {
+                                            "value": "#000000"
+                                            },
+                                            "enable": false,
+                                            "offset": {
+                                            "x": 0,
+                                            "y": 0
+                                            }
+                                        },
+                                        "shape": {
+                                            "options": {
+                                            "character": {
+                                                "value": [
+                                                "Design",
+                                                "Programming",
+                                                "Research"
+                                                ],
+                                                "font": "Roboto",
+                                                "style": "",
+                                                "weight": "400",
+                                                "fill": true
+                                            },
+                                            //   "image": {
+                                            //     "src": "https://cdn.matteobruni.it/images/particles/github.svg",
+                                            //     "width": 100,
+                                            //     "height": 100
+                                            //   },
+                                            //   "images": {
+                                            //     "src": "https://cdn.matteobruni.it/images/particles/github.svg",
+                                            //     "width": 100,
+                                            //     "height": 100
+                                            //   }
+                                            },
+                                            "type": "char"
+                                        },
+                                        "size": {
+                                            "random": {
+                                            "enable": false,
+                                            "minimumValue": 1
+                                            },
+                                            "value": 10,
+                                            "animation": {
+                                            "destroy": "none",
+                                            "enable": false,
+                                            "minimumValue": 10,
+                                            "speed": 10,
+                                            "startValue": "max",
+                                            "sync": false
+                                            }
+                                        },
+                                    },
+                                    "pauseOnBlur": true,
+                                    "pauseOnOutsideViewport": true,
+                                }}
+                            />
                     </Col>
                     <Col className="text-column" style={isNotPhone? experienceTextNotPhoneStyle : experienceTextForPhoneStyle} md={6} sm={10}>
                         <p>
@@ -229,6 +454,6 @@ const iconCloudStyle = {
     height: "20rem",
     backgroundColor: "rgba(49, 52, 66, 21)",
     borderRadius: "1rem",
-    padding: "2rem",
+    padding: "1rem",
     margin: "auto",
 }
